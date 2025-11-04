f35680490e91cc92c97f9a88c13e4f4f88454ff9
Rafael Bargiela <30897871+RafaBargiela@users.noreply.github.com>
2025-11-04 10:03:27 +0100

    Update L2.filter.Norm.R

--- Based_on_intensities/L2.filter.Norm.R
+++ Based_on_intensities/L2.filter.Norm.R
@@ -1,4 +1,4 @@
-# Author: Rafael Bargiela, PhD. Bangor University. 2021
+# Author: Rafael Bargiela, PhD. Instituto de Catálisis y Petroleoquímca, ICP (CSIC). Last update: 4th of November, 2025
 
 # Function for log2 transformation, groups filtering and Median Normalization.
 ################################################################
@@ -9,27 +9,32 @@
       # P: Matrix with raw proteins intensities.
       # groups.vector: Vector of length equal to number of columns in P, with the group assignation for each column (sample)
       # min.count.per : Minimum count of samples on each group where each protein should be present (default 60%, set 0 to include all proteins)
+      # include.small.groups : Logical. If TRUE, groups with n samples < 3 are included in the filtering, so when the expression of a protein meets the criteria within this groups, they are included in the final table, as it would happens with any other group. Default FALSE. 
 
-l2.filter.Norm<-function(P,groups.vector,min.count.per=60){
+l2.filter.Norm<-function(P,groups.vector,min.count.per=60,include.small.groups==FALSE){
       FM<-{}
-      l2P<-log(P,base=2) # Transforming RAW intensities to log2
+      l2P<-log(as.matrix(P),base=2) # Transforming RAW intensities to log2
       G<-unique(groups.vector) # Getting the different groups
       
       Gnames<-vector("character")
       for(g in 1:length(G)){
         group.names<-grep(G[g],groups.vector,value=FALSE,perl=TRUE) # Searching group on Metadata matrix (samples are by row)
-        M<-as.matrix(l2P[,group.names]) # Specific group log2 matrix
-        M[is.infinite(M)]<-0
-        
-        # Minimum count of samples of the group where protein should be present (set as 60%)
-        if(min.count.per==0){
-          min.count<-1 
+        if(include.small.groups==FALSE & length(group.names)<3){
+          message(paste("Group",G[g],"won't be considered for filtering due to n<3",sep=" "))
         }else{
-          min.count<-ceiling(ncol(M)*min.count.per/100) 
-        }
-        for(r in 1:nrow(M)){
-          if(sum(M[r,]!=0,na.rm=TRUE)>=min.count){
-            Gnames<-append(Gnames,rownames(M)[r],length(Gnames))
+          M<-as.matrix(l2P[,group.names]) # Specific group log2 matrix
+          M[is.infinite(M)]<-0
+          
+          # Minimum count of samples of the group where protein should be present (set as 60%)
+          if(min.count.per==0){
+            min.count<-1 
+          }else{
+              min.count<-ceiling(ncol(M)*min.count.per/100)
+          }
+          for(r in 1:nrow(M)){
+            if(sum(M[r,]!=0,na.rm=TRUE)>=min.count){
+              Gnames<-append(Gnames,rownames(M)[r],length(Gnames))
+            }
           }
         }
       }
